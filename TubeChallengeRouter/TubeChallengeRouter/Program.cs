f038076d6b0e7ce9e52e2a8b42383f63a009c5e3
Victor Yuchen Liu <victor.yuchen.liu@gmail.com>
2023-11-27 21:57:54 +0000

    test API working

--- TubeChallengeRouter/TubeChallengeRouter/Program.cs
+++ TubeChallengeRouter/TubeChallengeRouter/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-
-
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+using IO.Swagger.Model;
+using System.Linq;
 
 namespace TubeChallengeRouter
 {
@@ -9,6 +11,23 @@ namespace TubeChallengeRouter
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            TestAPI();
+        }
+
+        private static void TestAPI()
+        {
+            var apiconfig = new Configuration();
+            apiconfig.BasePath = "https://api.tfl.gov.uk";
+            var lineApi = new LineApi(apiconfig);
+            List<string> lines = new List<string>{"piccadilly", "northern"};
+            List<TflApiPresentationEntitiesLine> result = lineApi.LineGet(lines);
+            
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine($"Line {i}: {result[i].Name}");
+                List<TflApiPresentationEntitiesStopPoint> stations = lineApi.LineStopPoints(result[i].Id);
+                Console.WriteLine($"Stations: {string.Join(", ", stations.Select((item, index) => $"{item.CommonName}"))}");
+            }
         }
     }
 }
--- TubeChallengeRouter/TubeChallengeRouter/obj/Debug/net7.0/TubeChallengeRouter.AssemblyInfo.cs
+++ TubeChallengeRouter/TubeChallengeRouter/obj/Debug/net7.0/TubeChallengeRouter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TubeChallengeRouter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+367e7da06af461f2143193a1d703e7542fe6b58c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22ce6696bd5267e5e406a41e6fb89f3f044ccc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TubeChallengeRouter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TubeChallengeRouter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
