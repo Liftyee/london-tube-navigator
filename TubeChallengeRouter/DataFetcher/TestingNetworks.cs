8f79d4d56c432b582db1914cd19eab4248025ab2
Victor Yuchen Liu <victor.yuchen.liu@gmail.com>
2024-01-05 21:05:45 +0000

    Separate the TflModelWrapper class into separate file

---
+++ TubeChallengeRouter/DataFetcher/TestingNetworks.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Serilog.Core;
+using System.Diagnostics;
+using Serilog.Debugging;
+using TransportNetwork;
+
+
+namespace DataFetcher;
+
+public class LinearNetwork : INetworkDataFetcher
+{
+    private int numNodes;
+    public LinearNetwork(int nNodes)
+    {
+        numNodes = nNodes;
+    }
+    public void PopulateNetworkStructure(ref Network network)
+    {
+        for (int i = 0; i < numNodes; i++)
+        {
+            network.AddStationByIdIfNotPresent(i.ToString());
+        }
+
+        for (int i = 0; i < numNodes - 1; i++)
+        {
+            network.LinkStationsPartial(i.ToString(), (i + 1).ToString(), Dir.Inbound);
+            network.LinkStationsPartial((i+1).ToString(), i.ToString(), Dir.Outbound);
+        }
+    }
+}
+
+public class TestNetwork1 : INetworkDataFetcher
+{
+    public void PopulateNetworkStructure(ref Network _network)
+    {
+        _network.AddStation(new Station("A"));
+        _network.AddStation(new Station("B"));
+        _network.AddStation(new Station("C"));
+        _network.AddStation(new Station("D"));
+        _network.AddStation(new Station("E"));
+        _network.LinkStationsPartial("A", "B", Dir.Inbound, null);
+        _network.LinkStationsPartial("B", "C", Dir.Inbound, null);
+        _network.LinkStationsPartial("B", "D", Dir.Inbound, null);
+        _network.LinkStationsPartial("C", "E", Dir.Inbound, null);
+        _network.LinkStationsPartial("D", "E", Dir.Inbound, null);
+    }
+}
+
+public class InvalidBranchIDException : Exception
+{
+    public InvalidBranchIDException(string message) : base(message)
+    {
+    }
+}
--- TubeChallengeRouter/DataFetcher/TflModelWrapper.cs
+++ TubeChallengeRouter/DataFetcher/TflModelWrapper.cs
@@ -1,64 +1,12 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.Metrics;
+using System.Net;
+using System.Runtime.Serialization;
 using IO.Swagger.Api;
 using IO.Swagger.Client;
 using IO.Swagger.Model;
 using Serilog;
-using Serilog.Core;
-using System.Diagnostics;
-using System.Net;
-using Serilog.Debugging;
 using TransportNetwork;
-using System.Runtime.Serialization;
 
-    
 namespace DataFetcher;
-
-public class LinearNetwork : INetworkDataFetcher
-{
-    private int numNodes;
-    public LinearNetwork(int nNodes)
-    {
-        numNodes = nNodes;
-    }
-    public void PopulateNetworkStructure(ref Network network)
-    {
-        for (int i = 0; i < numNodes; i++)
-        {
-            network.AddStationByIdIfNotPresent(i.ToString());
-        }
-
-        for (int i = 0; i < numNodes - 1; i++)
-        {
-            network.LinkStationsPartial(i.ToString(), (i + 1).ToString(), Dir.Inbound);
-            network.LinkStationsPartial((i+1).ToString(), i.ToString(), Dir.Outbound);
-        }
-    }
-}
-
-public class TestNetwork1 : INetworkDataFetcher
-{
-    public void PopulateNetworkStructure(ref Network _network)
-    {
-        _network.AddStation(new Station("A"));
-        _network.AddStation(new Station("B"));
-        _network.AddStation(new Station("C"));
-        _network.AddStation(new Station("D"));
-        _network.AddStation(new Station("E"));
-        _network.LinkStationsPartial("A", "B", Dir.Inbound, null);
-        _network.LinkStationsPartial("B", "C", Dir.Inbound, null);
-        _network.LinkStationsPartial("B", "D", Dir.Inbound, null);
-        _network.LinkStationsPartial("C", "E", Dir.Inbound, null);
-        _network.LinkStationsPartial("D", "E", Dir.Inbound, null);
-    }
-}
-
-public class InvalidBranchIDException : Exception
-{
-    public InvalidBranchIDException(string message) : base(message)
-    {
-    }
-}
 
 public class TflModelWrapper : INetworkDataFetcher
 {
@@ -150,7 +98,7 @@ public class TflModelWrapper : INetworkDataFetcher
                         segment.BranchId);
                 }
             }
-            #warning "Need to add line information to the links!"
+#warning "Need to add line information to the links!"
         }
     }
 
