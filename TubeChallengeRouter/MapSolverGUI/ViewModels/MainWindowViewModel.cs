1deef53c144a226ceb5cf49e63410bba108190f9
Victor Yuchen Liu <victor.yuchen.liu@gmail.com>
2024-01-14 15:49:25 +0000

    working Avalonia tutorial, data bindings and ViewModel

--- TubeChallengeRouter/MapSolverGUI/ViewModels/MainWindowViewModel.cs
+++ TubeChallengeRouter/MapSolverGUI/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,63 @@
-﻿namespace MapSolverGUI.ViewModels;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MapSolverGUI.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+    public SimpleViewModel SimpleViewModel { get; } = new SimpleViewModel();
+    public ReactiveViewModel ReactiveViewModel { get; } = new ReactiveViewModel();
+}
+
+public class SimpleViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    
+    // [CallerMemberName] provides the property name for us so we don't have to when we RaisePropertyChanged();.
+    private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private string? _Name;
+
+    public string? Name
+    {
+        get
+        {
+            return _Name;
+        }
+        set
+        {
+            // only notify the UI if the name actually changed.
+            if (_Name != value)
+            {
+                _Name = value;
+                
+                // We call this to notify the UI about changes. 
+                RaisePropertyChanged();
+                
+                RaisePropertyChanged(nameof(Greeting));
+            }
+        }
+    }
+
+    public string Greeting
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                // If no Name is provided, use a default value.
+                return "Hello World from Avalonia.Samples";
+            }
+            else
+            {
+                return $"Hello {Name}";
+            }
+        }
+    }
 }
--- TubeChallengeRouter/MapSolverGUI/ViewModels/ViewModelBase.cs
+++ TubeChallengeRouter/MapSolverGUI/ViewModels/ViewModelBase.cs
@@ -1,7 +1,50 @@
-﻿using ReactiveUI;
+﻿using System;
+using ReactiveUI;
 
 namespace MapSolverGUI.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
+}
+
+public class ReactiveViewModel : ReactiveObject
+{
+    public ReactiveViewModel()
+    {
+        // We can listen to any property changes with "WhenAnyValue" and do whatever we want in "Subscribe".
+        this.WhenAnyValue(o => o.Name)
+            .Subscribe(new Action<object>(o => this.RaisePropertyChanged(nameof(Greeting))));
+    }
+    
+    private string? _Name;
+
+    public string? Name
+    {
+        get
+        {
+            return _Name;
+        }
+        set
+        {
+            // ReactiveUI function to raise (notify) the UI if anything changed and also set our backing field
+            this.RaiseAndSetIfChanged(ref _Name, value);
+        }
+    }
+    
+    public string Greeting
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                // If no Name is provided, use a default Greeting
+                return "Hello World from Avalonia.Samples";
+            }
+            else
+            {
+                // else greet the User.
+                return $"Hello {Name}";
+            }
+        }
+    }
 }
