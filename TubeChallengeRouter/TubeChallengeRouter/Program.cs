2956665042eaef103a5ae6f5f9c18d4cfa7a7fcf
Victor Yuchen Liu <victor.yuchen.liu@gmail.com>
2024-01-16 22:38:50 +0000

    remove unused tfl-csharp-client references

--- TubeChallengeRouter/TubeChallengeRouter/Program.cs
+++ TubeChallengeRouter/TubeChallengeRouter/Program.cs
@@ -1,14 +1,7 @@
-﻿using System.Diagnostics;
-using System.Text;
+﻿using System.Text;
 using DataFetcher;
-using IO.Swagger.Api;
-using IO.Swagger.Client;
-using IO.Swagger.Model;
-using NUnit.Framework.Constraints;
 using TransportNetwork;
 using Serilog;
-using DataFetcher;
-using Eto.Forms;
 using RouteSolver;
 
 namespace TubeChallengeRouter
@@ -28,33 +21,6 @@ namespace TubeChallengeRouter
             //TestTubeGenFloyd();
         }
         
-        private static void TestAPI()
-        {
-            var apiconfig = new Configuration();
-            apiconfig.BasePath = "https://api.tfl.gov.uk";
-            var lineApi = new LineApi(apiconfig);
-
-            logger.Debug("Fetching all tube lines...");
-            var rawLines = lineApi.LineGetByMode(new List<string> { "tube" });
-            
-            List<string> lineIds = new List<string>();
-            foreach (var l in rawLines)
-            {
-                lineIds.Add(l.Id);
-            }
-            logger.Information($"Found lines: {string.Join(", ", lineIds)}");
-            
-            for (int i = 0; i < lineIds.Count; i++)
-            {
-                logger.Debug($"Line {i}: {lineIds[i]}");
-                var lineStationSequences = lineApi.LineRouteSequence(lineIds[i],"inbound");
-                logger.Debug($"{lineStationSequences.StopPointSequences.Count} station segments found");
-                for (int j = 0; j < lineStationSequences.StopPointSequences.Count; j++)
-                {
-                    logger.Debug($"Segment {j}: {lineStationSequences.StopPointSequences[j].StopPoint[0].Name} to {lineStationSequences.StopPointSequences[j].StopPoint.Last().Name}");
-                }
-            }
-        }
         private static void TestTubeGen()
         {
             NetworkFactory tubeFactory = new NetworkFactory(new TflModelWrapper(logger, GetCachePath()));
