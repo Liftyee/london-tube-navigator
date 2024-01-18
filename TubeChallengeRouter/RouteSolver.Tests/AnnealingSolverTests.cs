70c0b10df46f7598a06129a25f805cb3c8729324
Victor Liu <victor.yuchen.liu@gmail.com>
2024-01-18 09:52:36 +0000

    init unit tests for Solver

---
+++ TubeChallengeRouter/RouteSolver.Tests/AnnealingSolverTests.cs
@@ -0,0 +1,15 @@
+namespace RouteSolver.Tests;
+
+public class SolverTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
