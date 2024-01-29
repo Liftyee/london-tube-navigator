415d3c30bbcbc447a227d80c75ebd6aa2b95c88b
Victor Liu <victor.yuchen.liu@gmail.com>
2024-01-29 15:03:50 +0000

    fix code style in viewmodel file

--- TubeChallengeRouter/MapSolverGUI/ViewModels/SolverControlViewModel.cs
+++ TubeChallengeRouter/MapSolverGUI/ViewModels/SolverControlViewModel.cs
@@ -3,15 +3,12 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using Avalonia.Controls;
 using DataFetcher;
 using ReactiveUI;
 using RouteSolver;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
-using SkiaSharp;
-using SkiaSharp.HarfBuzz;
 using TransportNetwork;
 
 namespace MapSolverGUI.ViewModels;
@@ -23,109 +20,88 @@ public class SolverControlViewModel : ReactiveObject
     private double _swapProb;
     private double _tempFactor;
     private int _maxIterations;
-    private ISolver solver;
+    private readonly ISolver _solver;
     public ICommand SolveCommand { get; }
     public ICommand TestControls { get; }
     public ObservableCollection<string> OutputLog { get; } = new ObservableCollection<string>();
     
     private static UIOutputSink UILogger { get; } = new UIOutputSink();
-    private static ILogger logger = new LoggerConfiguration()
+    private static readonly ILogger Logger = new LoggerConfiguration()
         .MinimumLevel.Debug()
         .WriteTo.Console()
         .WriteTo.Sink(UILogger, LogEventLevel.Information)
         .CreateLogger();
     
-    internal class UIOutputSink : ILogEventSink
+    private class UIOutputSink : ILogEventSink
     {
-        private ObservableCollection<string>? outputLog;
+        private ObservableCollection<string>? _outputLog;
         
         public void Emit(LogEvent logEvent)
         {
-            if (outputLog == null)
+            if (_outputLog == null)
             {
                 throw new NullReferenceException("Tried to log but Output log not set");
             }
             
-            outputLog.Add(logEvent.RenderMessage());
+            _outputLog.Add(logEvent.RenderMessage());
         }
 
         public void AddOutput(ObservableCollection<string> output)
         {
-            outputLog = output;
+            _outputLog = output;
         }
     }
 
     public double SolveProgress
     {
-        get
-        {
-            return _solveProgress;
-        }
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _solveProgress, value);
-        }
+        get => _solveProgress;
+        set => this.RaiseAndSetIfChanged(ref _solveProgress, value);
     }
 
     public string? StartStation
     {
-        get
-        {
-            return _startStationName;
-        }
-        set
-        {
-            this.RaiseAndSetIfChanged(ref _startStationName, value);
-        }
+        get => _startStationName;
+        set => this.RaiseAndSetIfChanged(ref _startStationName, value);
     }
     
     public double SwapProb
     {
-        get
-        {
-            return _swapProb;
-        }
+        get => _swapProb;
         set
         {
-            solver.SetRandomSwapProbability(value);
+            _solver.SetRandomSwapProbability(value);
             this.RaiseAndSetIfChanged(ref _swapProb, value);
         }
     }
     
     public double TempFactor
     {
-        get
-        {
-            return _tempFactor;
-        }
+        get => _tempFactor;
         set
         {
-            solver.SetCoolDownFactor(value);
+            _solver.SetCoolDownFactor(value);
             this.RaiseAndSetIfChanged(ref _tempFactor, value);
         }
     }
     
     public int MaxIterations
     {
-        get
-        {
-            return _maxIterations;
-        }
+        get => _maxIterations;
         set
         {
-            solver.SetMaxIterations(value);
+            _solver.SetMaxIterations(value);
             this.RaiseAndSetIfChanged(ref _maxIterations, value);
         }
     }
 
     public SolverControlViewModel()
     {
-        logger.Information("Hello World! Logging is {Description}.","online");
+        Logger.Information("Hello World! Logging is {Description}.","online");
         UILogger.AddOutput(OutputLog);
-        solver = new AnnealingSolver(logger, SetProgress);
-        SwapProb = solver.GetRandomSwapProbability();
-        TempFactor = solver.GetCoolDownFactor();
-        MaxIterations = solver.GetMaxIterations();
+        _solver = new AnnealingSolver(Logger, SetProgress);
+        SwapProb = _solver.GetRandomSwapProbability();
+        TempFactor = _solver.GetCoolDownFactor();
+        MaxIterations = _solver.GetMaxIterations();
 
         SolveCommand = ReactiveCommand.CreateFromTask(SolveRouteAsync);
         TestControls = ReactiveCommand.CreateFromTask(TestOutputs);
@@ -133,34 +109,25 @@ public class SolverControlViewModel : ReactiveObject
 
     private void RunSolve()
     {
-        INetworkDataFetcher fetcher = new TflModelWrapper(logger, GetCachePath());
+        INetworkDataFetcher fetcher = new TflModelWrapper(Logger, GetCachePath());
         fetcher.SetProgressCallback(SetProgress);
         NetworkFactory tubeFactory = new NetworkFactory(fetcher);
         Network tube;
         try
         {
-            tube = tubeFactory.Generate(NetworkType.Dijkstra, logger);
+            tube = tubeFactory.Generate(NetworkType.Dijkstra, Logger);
         }
         catch (IO.Swagger.Client.ApiException)
         {
-            logger.Error("Could not fetch Tube network data. Try checking your internet connection.");
+            Logger.Error("Could not fetch Tube network data. Try checking your internet connection.");
             throw new Exception("Couldn't fetch data from API.");
         }
-        logger.Debug("Result: {A}",tube.ToString());
+        Logger.Debug("Result: {A}",tube.ToString());
         
-        Route route = solver.Solve(tube);
-        logger.Debug("Route: {A} (duration {B})",tube.RouteToStringStationSeq(route), route.Duration);
+        Route route = _solver.Solve(tube);
+        Logger.Debug("Route: {A} (duration {B})",tube.RouteToStringStationSeq(route), route.Duration);
 
-        // TODO: extract this output code to a function
-        var now = DateTime.Now;
-        string datecode = $"{now.Year}-{now.Month}-{now.Day}_{now.Hour}-{now.Minute}";
-        string outputpath = $"{GetCachePath()}route{datecode}.txt";
-        // write route to a file
-        using (var file = new FileStream(outputpath, FileMode.Create))
-        {
-            tube.RouteDetailsToStream(route, file);
-            logger.Information("Result written to {A}", outputpath);
-        }
+        WriteRouteToFile(tube, route);
         ShowSolverResult(route);
     }
 
@@ -177,15 +144,15 @@ public class SolverControlViewModel : ReactiveObject
 
     private async Task SolveRouteAsync()
     {
-        OutputLog.Add($"Generating route with {MaxIterations} iterations and temperature factor {TempFactor.ToString("0.###")}...");
+        OutputLog.Add($"Generating route with {MaxIterations} iterations and temperature factor {TempFactor:0.###}...");
         try
         {
-            await Task.Run(() => RunSolve());
+            await Task.Run(RunSolve);
         }
         catch (Exception e)
         {
             OutputLog.Add($"Error while solving: {e.Message}");
-            logger.Debug("Exception while solving: {A}", e);
+            Logger.Debug("Exception while solving: {A}", e);
         }
     }
 
@@ -205,5 +172,18 @@ public class SolverControlViewModel : ReactiveObject
         string homeDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         const string furtherPath = ".cache/TubeNetworkCache/"; // Linux standard but works for Windows too
         return Path.Combine(homeDir, furtherPath);
+    }
+
+    private void WriteRouteToFile(Network tube, Route route)
+    {
+        // generate a unique filename
+        var now = DateTime.Now;
+        string dateCode = $"{now.Year}-{now.Month}-{now.Day}_{now.Hour}-{now.Minute}";
+        string outputPath = $"{GetCachePath()}route{dateCode}.txt";
+
+        // write the route directly to the filestream
+        using var file = new FileStream(outputPath, FileMode.Create);
+        tube.RouteDetailsToStream(route, file);
+        Logger.Information("Result written to {A}", outputPath);
     }
 }
