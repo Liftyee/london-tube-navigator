71244b914cce8424e226273909c1dca5850ff879
Victor Liu <victor.yuchen.liu@gmail.com>
2024-03-13 20:47:51 +0000

    start writing basic solver tests

--- TubeChallengeRouter/RouteSolver.Tests/AnnealingSolverTests.cs
+++ TubeChallengeRouter/RouteSolver.Tests/AnnealingSolverTests.cs
@@ -2,14 +2,31 @@ namespace RouteSolver.Tests;
 
 public class SolverTests
 {
+    private Network _net;
+    private AnnealingSolver _solver;
     [SetUp]
     public void Setup()
     {
+        ILogger stubLogger = new LoggerConfiguration().CreateLogger();
+        NetworkFactory factory = new NetworkFactory(new LinearNetwork(10));
+        _net = factory.Generate(NetworkType.Floyd, stubLogger);
+        _solver = new AnnealingSolver(stubLogger);
+        
     }
 
     [Test]
-    public void Test1()
+    public void Solver_GeneratesSolution()
     {
-        Assert.Pass();
+        Route result = _solver.Solve(_net);
+        Assert.That(result, Is.Not.Null);
+    }
+
+    [Test]
+    public void Solver_SettingsChange()
+    {
+        _solver.SetMaxIterations(50000);
+        
+        Route result = _solver.Solve(_net);
+        Assert.That(result, Is.Not.Null);
     }
 }
--- TubeChallengeRouter/RouteSolver.Tests/Usings.cs
+++ TubeChallengeRouter/RouteSolver.Tests/Usings.cs
@@ -1,1 +1,5 @@
 global using NUnit.Framework;
+global using TransportNetwork;
+global using RouteSolver;
+global using DataFetcher;
+global using Serilog;
