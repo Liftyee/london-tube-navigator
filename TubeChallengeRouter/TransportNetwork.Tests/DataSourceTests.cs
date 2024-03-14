1a9a4b0091047a8230b146f40aa9093b1bdc3d5a
Victor Liu <victor.yuchen.liu@gmail.com>
2024-03-14 11:08:26 +0000

    Test Linear Network data source

--- TubeChallengeRouter/TransportNetwork.Tests/DataSourceTests.cs
+++ TubeChallengeRouter/TransportNetwork.Tests/DataSourceTests.cs
@@ -3,21 +3,32 @@ namespace StationTests;
 [TestFixture]
 public class DataSourceTests
 {
+    private ILogger stubLogger;
     [SetUp]
     public void Setup()
     {
-        
+        stubLogger = new LoggerConfiguration().CreateLogger();
     }
 
     [Test]
     public void TfLDataSource_PopulatesNetwork()
     {
-        ILogger stubLogger = new LoggerConfiguration().CreateLogger();
         Network tubeNetwork = new NetworkFactory(
                 new TflModelWrapper(stubLogger, "./"))
                             .Generate(NetworkType.Dijkstra, stubLogger);
         
         // As of Mar 2024, the Tube network has 273 station IDs
         Assert.That(tubeNetwork.GetStationIDs().Count, Is.EqualTo(273)); 
+    }
+
+    [Test]
+    public void LinearNetwork_CorrectNumNodes()
+    {
+        for (int i = 1; i < 20; i += 5)
+        {
+            Network linearNetwork = new NetworkFactory(new LinearNetwork(i))
+                .Generate(NetworkType.Dijkstra, stubLogger);
+            Assert.That(linearNetwork.GetStationIDs().Count, Is.EqualTo(i));
+        }
     }
 }
