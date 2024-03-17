9adcc6b33bc21d68988d366318b1d0dc07c7e95f
Victor Yuchen Liu <victor.yuchen.liu@gmail.com>
2024-03-17 21:13:39 +0000

    Simplify Swap() and add comments

--- TubeChallengeRouter/TransportNetwork/DijkstraCostNetwork.cs
+++ TubeChallengeRouter/TransportNetwork/DijkstraCostNetwork.cs
@@ -23,16 +23,17 @@ public class DijkstraCostNetwork : Network
         }
     }
     
-    private Dictionary<string, Dictionary<string, int?>> _costCache;
-    private Dictionary<string, Dictionary<string, List<string>>> _pathCache;
-    
     public DijkstraCostNetwork(ILogger logger) : base(logger)
     {
     }
+    
+    private Dictionary<string, Dictionary<string, int?>> _costCache;
+    private Dictionary<string, Dictionary<string, List<string>>> _pathCache;
 
+    // Override the base Network class's method to set up the cost cache
     internal override void Initialise()
     {
-        // initialise cost matrix to all null's
+        // Initialise cost and path matrix to default values
         _costCache = new Dictionary<string, Dictionary<string, int?>>();
         _pathCache = new Dictionary<string, Dictionary<string, List<string>>>();
         foreach (string stationId in Stations.Keys)
@@ -54,6 +55,10 @@ public class DijkstraCostNetwork : Network
         }
     }
     
+    // Function to return the minimum cost of a path between two stations
+    // as well as the path itself, using cached results if possible
+    // The path is returned by the out parameter "path"
+    // Overrides and implements base class abstract method 
     public override int CostFunction(string startId, string endId, out List<string> path)
     {
         // if it's a direct path, just return nothing!
@@ -72,7 +77,7 @@ public class DijkstraCostNetwork : Network
         
         // otherwise, calculate it and update the cache
         _costCache[startId][endId] = DijkstraLookup(startId, endId, out var result);
-        _pathCache[startId][endId] = result; // TODO: might induce undesired referencing? 
+        _pathCache[startId][endId] = result;
         path = result;
 
         if (_costCache[startId][endId]!.Value < 0)
@@ -82,25 +87,38 @@ public class DijkstraCostNetwork : Network
         return _costCache[startId][endId]!.Value;
     }
 
+    // Implement the simpler version of the CostFunction method too
     public override int CostFunction(string startId, string endId)
     {
         List<string> _ = new List<string>();
         return CostFunction(startId, endId, out _);
     }
-
+    
+    // Use Dijkstra's algorithm to find the shortest path between two 
+    // stations. Return the cost of that path, and the path itself via the 
+    // out parameter "path".
     private int DijkstraLookup(string startId, string endId, out List<string> path)
     {
+        // If the start and end are the same, just return 0
         if (startId == endId)
         {
             path = new List<string>();
             return 0;
         }
         
+        // Store the previous station which led to each station, to 
+        // reconstruct the path
         Dictionary<string, string> prev = new();
+        HashSet<string> visited = new(); // Use a HashSet for O(1) lookups
+        
+        // Priority queue for quick access to the least cost (next) station
         PriorityQueue<DijkstraNode>
             nextNodes = new PriorityQueue<DijkstraNode>(Stations.Count + 20, Priority.Smallest);
+        
+        // Prime the queue with our start station
         nextNodes.Insert(new DijkstraNode(startId, 0));
-        HashSet<string> visited = new();
+        
+        // Initialise a dict to store the cost to each station
         Dictionary<string, int> dist = new();
         foreach (string stationId in Stations.Keys)
         {
@@ -111,6 +129,7 @@ public class DijkstraCostNetwork : Network
         while (nextNodes.Count > 0)
         {
             DijkstraNode minCostNode = nextNodes.Pop();
+            if (visited.Contains(minCostNode.StationId)) continue;
             foreach (Link link in Stations[minCostNode.StationId].GetLinks())
             {
                 if (visited.Contains(link.Destination.NaptanId)) continue;
@@ -124,28 +143,28 @@ public class DijkstraCostNetwork : Network
                 }
             }
             visited.Add(minCostNode.StationId);
+            
+            // If we're at the end, reconstruct the path and return
             if (minCostNode.StationId == endId)
             {
-                // reconstruct path
-                List<string> result = new();
+                // Get path by starting at the end and backtracking
+                path = new();
                 string? current = prev[endId];
                 while (current != startId)
                 {
-                    result.Add(current);
+                    path.Add(current);
                     current = prev[current];
                 }
-                result.Reverse();
-                path = result;
-                Debug.Assert(!path.Contains(startId));
-                Debug.Assert(!path.Contains(endId));
+                path.Reverse(); // We backtracked, so reverse the list
                 return minCostNode.Cost;
             }
         }
-        Logger.Debug("No more stations to visit");
-        // TODO: make a custom exception
+        // If there is a path between the stations, we should never get here
+        Logger.Error("Dijkstra: No more stations to visit!");
         throw new ArgumentException($"No route found between {startId} and {endId}");
     }
     
+    // Override base class method so the intermediates are updated properly
     public override void Swap(ref Route route, int idxA, int idxB)
     {
         List<string> stations = route.TargetStations;
@@ -153,39 +172,24 @@ public class DijkstraCostNetwork : Network
         int updatedCost = route.Cost;
         int maxIdx = route.Count - 1;
         
-        /* Instead of recalculating the travel time by summing all travel times between stations, we can just
-           change the travel times to and from the stations that are being swapped. All other travel times should
-           remain constant, so we are only concerned with what happens around our swapped stations. */
-
-        void UpdateCostsTo(int statIdx)
-        {
-            if (statIdx > 0)
-            {
-                updatedCost -= CostFunction(stations[statIdx - 1], stations[statIdx]);
-            }
-            
-            if (statIdx < maxIdx)
-            {
-                updatedCost -= CostFunction(stations[statIdx], stations[statIdx + 1]);
-            }
-        }
+        // These four costs are the costs to travel to and from the
+        // stations being swapped (before the swap)
+        if (idxA > 0)      updatedCost -= CostFunction(route, idxA-1, idxA);
+        if (idxA < maxIdx) updatedCost -= CostFunction(route, idxA, idxA+1);
+        if (idxB > 0)      updatedCost -= CostFunction(route, idxB - 1, idxB);
+        if (idxB < maxIdx) updatedCost -= CostFunction(route, idxB, idxB + 1);
         
-        UpdateCostsTo(idxA);
-        UpdateCostsTo(idxB);
-        
-        // swap the stations in the route
+        // Swap the stations in the route
         string temp = stations[idxA];
         stations[idxA] = stations[idxB];
         stations[idxB] = temp;
         
-        // Now add the updated cost to travel to/from the newly swapped stations
-        if (idxA > 0)               updatedCost += UpdatePathReturnCost(route, idxA);
-        if (idxA < route.Count - 1) updatedCost += UpdatePathReturnCost(route, idxA + 1);
-        if (idxB > 0)               updatedCost += UpdatePathReturnCost(route, idxB);
-        if (idxB < route.Count - 1) updatedCost += UpdatePathReturnCost(route, idxB + 1);
+        // Now add the updated cost to travel to/from the swapped stations
+        if (idxA > 0)      updatedCost += UpdatePath(route, idxA);
+        if (idxA < maxIdx) updatedCost += UpdatePath(route, idxA + 1);
+        if (idxB > 0)      updatedCost += UpdatePath(route, idxB);
+        if (idxB < maxIdx) updatedCost += UpdatePath(route, idxB + 1);
         
-        
-        // update the route's cost (unitless value based on number of factors)
         route.UpdateCost(updatedCost);
     }
     
@@ -262,19 +266,19 @@ public class DijkstraCostNetwork : Network
             if (insertBefore > 0)
             {
                 Logger.Verbose("Adding cost from {A} to {B}, {C}", insertBefore-1, insertBefore, CostFunction(stations[insertBefore - 1], stations[insertBefore]));
-                updatedCost += UpdatePathReturnCost(route, insertBefore);
+                updatedCost += UpdatePath(route, insertBefore);
             }
             
             if (insertBefore > 1)
             {
                 Logger.Verbose("Adding cost from {A} to {B}, {C}", insertBefore-2, insertBefore-1, CostFunction(stations[insertBefore - 2], stations[insertBefore-1]));
-                updatedCost += UpdatePathReturnCost(route, insertBefore-1);
+                updatedCost += UpdatePath(route, insertBefore-1);
             }
             
             if (takeFrom > 0)
             {
                 Logger.Verbose("Adding travel time from {A} to {B}", takeFrom-1, takeFrom);
-                updatedCost += UpdatePathReturnCost(route, takeFrom);
+                updatedCost += UpdatePath(route, takeFrom);
             }
         }
         else
@@ -284,19 +288,19 @@ public class DijkstraCostNetwork : Network
             if (insertBefore > 0)
             {
                 Logger.Verbose("Adding travel time from {A} to {B}", insertBefore-1, insertBefore);
-                updatedCost += UpdatePathReturnCost(route, insertBefore);
+                updatedCost += UpdatePath(route, insertBefore);
             }
         
             if (insertBefore < route.Count-1)
             {
                 Logger.Verbose("Adding travel time from {A} to {B}", insertBefore, insertBefore+1);
-                updatedCost += UpdatePathReturnCost(route, insertBefore+1);
+                updatedCost += UpdatePath(route, insertBefore+1);
             }
 
             if (takeFrom < route.Count-1)
             {
                 Logger.Verbose("Adding travel time from {A} to {B}", takeFrom, takeFrom+1);
-                updatedCost += UpdatePathReturnCost(route, takeFrom);
+                updatedCost += UpdatePath(route, takeFrom);
             }
 
         }
@@ -308,9 +312,11 @@ public class DijkstraCostNetwork : Network
         Logger.Verbose("Route is {A}", route.ToString());
     }
 
-    // Function to update the intermediate station path of an indexed segment of a Route
-    // Deduplicates code in Swap() and TakeInsert() functions
-    private int UpdatePathReturnCost(Route route, int idxA)
+    /* Function to update the intermediate station path between two stations
+       A and B, where B is the station at index idxA in the route and A 
+       is at idxA-1. Returns the cost of the new path.
+       Deduplicates code in Swap() and TakeInsert() functions */
+    private int UpdatePath(Route route, int idxA)
     {
         List<string> newpath;
         int cost = CostFunction(route.TargetStations[idxA - 1], route.TargetStations[idxA], out newpath);
